bd89750d367489486290aefc0842696c5ebf5c7a
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2021-05-05 16:22:49 +0100

    added created_at

--- controllers/RecordController.js
+++ controllers/RecordController.js
@@ -130,7 +130,9 @@ const graphData = async (req, res, next) => {
                 [sequelize.fn("avg", sequelize.col("level")), "y"],
             ],
             group: [sequelize.literal(`DATE(created_at)`)],
-            order: ['created_at', 'ASC'],
+            order: [
+                [sequelize.literal(`DATE(created_at)`), "ASC"]
+            ],
             raw: true,
         });
         if (records.length) {

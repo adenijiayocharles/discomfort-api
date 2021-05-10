14e4d7826bcf3b3241bb84995144ad563a468ed1
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2021-05-10 12:20:42 +0100

    fixed post

--- routes/post.js
+++ routes/post.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const postController = require("../controllers/PostController");
 const authMiddlware = require("../middleware/auth");
 
+router.get("/user", authMiddlware, postController.userPosts);
 router.get("/:post_id", authMiddlware, postController.one);
 router.get("/", authMiddlware, postController.all);
-router.get("/user", authMiddlware, postController.userPosts);
 router.post("/", authMiddlware, postController.create);
 router.delete("/:post_id", authMiddlware, postController.deletePost);
 

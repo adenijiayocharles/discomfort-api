1aad8410d78467931411e0b0edb81dc8f4868966
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2021-05-20 13:19:00 +0100

    Added user login test

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -51,7 +51,6 @@ const login = async (req, res, next) => {
         const user = await User.findOne({
             where: { email: req.body.email },
         });
-
         if (!user) {
             return handleErrorResponse({
                 res,

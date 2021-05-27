cf9c9f04bfa9a1b67b14f14125a1d950c0e537ab
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2021-05-27 05:38:54 +0100

    fixes

--- controllers/RecordController.js
+++ controllers/RecordController.js
@@ -42,7 +42,7 @@ const all = async (req, res, next) => {
             where: {
                 user_id: req.user_details.data.id,
             },
-            order: [['sequelize.literal(`DATE(created_at)`)', "DESC"]],
+            order: [[sequelize.literal(`DATE(created_at)`), "DESC"]]
         });
         if (records.length) {
             return handleSuccessResponse({
